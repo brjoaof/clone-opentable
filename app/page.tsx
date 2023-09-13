5512bf0abae3f40050b69d7bde0e776bfe0a6511
João Felipe <65171379+brjoaof@users.noreply.github.com>
2023-09-13 19:38:39 +0000

    Adiciona 'html' das páginas

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import styles from './page.module.css'
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1 className='text-blue-400 text-7xl'>Hello Next.js 13</h1>
-    </main>
-  )
+  return <main></main>;
 }
