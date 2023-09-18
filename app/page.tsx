fd78e8a66a8e330608a03cf10d9cf4bd0333d75f
João Felipe <65171379+brjoaof@users.noreply.github.com>
2023-09-18 18:26:52 +0000

    fetching data and rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { Cuisine, Location, PRICE, PrismaClient } from "@prisma/client";
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  slug: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async () => {
+  const restaurants = prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      slug: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  });
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
+  console.log(restaurants);
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </main>
   );
